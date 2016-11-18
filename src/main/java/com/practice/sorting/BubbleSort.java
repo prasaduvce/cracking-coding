b6b6a7e33037b68cd33ae6ffbe883a443f9cebcf
renuka.prasad <renuka.prasad@rakuten.com>
2016-11-18 19:38:05 +0900

    Bubble Sort Technique

---
+++ src/main/java/com/practice/sorting/BubbleSort.java
@@ -0,0 +1,37 @@
+package com.practice.sorting;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] num = new int[20];
+		int temp;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number of elements");
+		int n = scanner.nextInt();
+		System.out.println("enter array elements one by one");
+		for (int i=0; i<n; i++) {
+			num[i] = scanner.nextInt();
+		}
+		System.out.println("Unsorted array elements below");
+		for (int i=0; i<n; i++) {
+			System.out.println(num[i]);
+		}
+		for (int i=0; i<n; i++) {
+			for (int j=1; j<n-i;j++) {
+				if (num[j-1] > num[j]) {
+					temp = num[j-1];
+					num[j-1] = num[j];
+					num[j] = temp;
+				}
+			}
+		}
+		System.out.println("Sorted array elements below");
+		for (int i=0; i<n; i++) {
+			System.out.println(num[i]);
+		}
+	}
+
+
+}
