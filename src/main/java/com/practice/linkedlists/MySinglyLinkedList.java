c7dd567458e1f46f3e843fdf0ebeb07aaacbd90f
renuka.prasad <renuka.prasad@rakuten.com>
2017-04-20 00:52:40 +0900

    Linked List examples

---
+++ src/main/java/com/practice/linkedlists/MySinglyLinkedList.java
@@ -0,0 +1,72 @@
+package com.practice.linkedlists;
+
+class Node1 {
+	int data;
+	Node1 link;
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node1 getLink() {
+		return link;
+	}
+
+	public void setLink(Node1 link) {
+		this.link = link;
+	}
+}
+
+public class MySinglyLinkedList {
+
+	private Node1 head;
+	public Node1 insertToEnd(int item) {
+		Node1 node = new Node1();
+		node.setData(item);
+		node.setLink(null);
+
+		if (head == null) {
+			head = node;
+			return head;
+		}
+		Node1 cur = head;
+		while (cur.getLink() != null) {
+			cur = cur.getLink();
+		}
+		cur.setLink(node);
+		return head;
+	}
+
+	public Node1 insertFront(int item) {
+		Node1 newNode = new Node1();
+		newNode.setData(item);
+		newNode.setLink(null);
+
+		if (head == null) {
+			head = newNode;
+			return head;
+		}
+
+		newNode.setLink(head);
+		head = newNode;
+		return head;
+	}
+
+
+
+	public void display() {
+		if (head == null) {
+			System.out.println("Empty Linked list");
+			return;
+		}
+
+		for (Node1 tmp=head;tmp != null; tmp=tmp.getLink()) {
+			System.out.print(tmp.getData()+" ");
+		}
+
+	}
+}
---
+++ src/main/java/com/practice/linkedlists/TestMySinglyLinkedList.java
@@ -0,0 +1,17 @@
+package com.practice.linkedlists;
+
+
+public class TestMySinglyLinkedList {
+	public static void main(String[] args) {
+
+		MySinglyLinkedList linkedList = new MySinglyLinkedList();
+		linkedList.display();
+		linkedList.insertToEnd(33);
+		linkedList.insertToEnd(66);
+		linkedList.insertFront(22);
+		linkedList.insertFront(11);
+		linkedList.insertToEnd(44);
+		linkedList.insertToEnd(55);
+		linkedList.display();
+	}
+}
