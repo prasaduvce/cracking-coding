27b5d4ba315e8997d0ac71dc807820d4b7a27fc8
Prasad, Renuka | RIEPL <renuka.prasad@rakuten.com>
2018-09-12 10:03:31 +0530

    Odd occurance map.

--- src/main/java/com/practice/codility/lessons/OddOccuranceInArray.java
+++ src/main/java/com/practice/codility/lessons/OddOccuranceInArray.java
@@ -2,10 +2,11 @@ package com.practice.codility.lessons;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class OddOccuranceInArray {
 	public static void main(String[] args) {
-		int[] inputArray = {9, 3, 9, 3, 9, 7, 9, 9};
+		int[] inputArray = {9, 3, 9, 3, 9, 9, 9};
 		Map<Integer, Integer> integerMap = new HashMap<>();
 
 		for (int num : inputArray) {
@@ -17,10 +18,18 @@ public class OddOccuranceInArray {
 			}
 		}
 
-		integerMap.forEach((key, value) -> {
+		/*integerMap.forEach((key, value) -> {
 			if (value % 2 != 0) {
 				System.out.println("Number " + key + " has odd occurance.");
 			}
-		});
+		});*/
+
+		Integer oddOccurance = null;
+		Optional<Map.Entry<Integer, Integer>> first = integerMap.entrySet().stream().filter(integerCountEntry -> integerCountEntry.getValue() % 2 != 0).findFirst();
+
+		if (first.isPresent()) {
+			oddOccurance = first.get().getKey();
+		}
+		System.out.println("Number " + oddOccurance + " has odd occurance.");
 	}
 }
