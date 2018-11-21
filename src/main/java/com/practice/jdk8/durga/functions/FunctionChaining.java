cd0a7c7b8d165142ba06e5fcbba2eea906e217a5
Prasad, Renuka | RIEPL <renuka.prasad@rakuten.com>
2018-11-21 17:05:09 +0530

    Java 8 function chaining.

--- src/main/java/com/practice/jdk8/durga/functions/FunctionChaining.java
+++ src/main/java/com/practice/jdk8/durga/functions/FunctionChaining.java
@@ -6,11 +6,15 @@ public class FunctionChaining {
 	public static void main(String[] args) {
 		Function<String, String> f1 = s -> s.toUpperCase();
 		Function<String, String> f2 = s -> s.substring(0, 9);
-
 		System.out.println(f1.apply("Aishwaryaabhi"));
 		System.out.println(f2.apply("Aishwaryaabhi"));
 		System.out.println(f1.andThen(f2).apply("Aishwaryaabhi"));
 		System.out.println(f1.compose(f2).apply("Aishwaryaabhi"));
+
+		Function<Integer, Integer> f1new = i -> i + i;
+		Function<Integer, Integer> f2new = i -> i * i * i;
+		System.out.println(f1new.andThen(f2new).apply(2));
+		System.out.println(f1new.compose(f2new).apply(2));
 	}
 
 }
