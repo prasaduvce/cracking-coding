526639dea171652bb4fe74c546d5d4e899802c43
prasad <renuka.bandur@omio.com>
2020-04-24 15:19:06 +0200

    Largest fibonacci subset

--- src/main/java/com/practice/facebook/LargestFibonacciSubset.java
+++ src/main/java/com/practice/facebook/LargestFibonacciSubset.java
@@ -3,22 +3,41 @@ package com.practice.facebook;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.OptionalInt;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class LargestFibonacciSubset {
 
     public static void main(String[] args) {
-        int arr[] = {1, 4, 3, 9, 10, 13, 7};
+        List<List<Integer>> output = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int numberOfTests = scanner.nextInt();
+        while (numberOfTests > 0) {
+            int n = scanner.nextInt();
+            int nums[] = new int[n];
+            for (int i = 0; i<n; i++) {
+                nums[i] = scanner.nextInt();
+            }
+            output.add(findSubset(nums));
+            numberOfTests--;
+        }
+        for(int i=0; i<output.size(); i++) {
+            List<Integer> rowList = output.get(i);
+            for (int j=0;j< rowList.size(); j++) {
+                System.out.print(rowList.get(j)+" ");
+            }
+            System.out.println();
+        }
+    }
 
+    private static List<Integer> findSubset(int[] arr) {
         OptionalInt maxNumber = Arrays.stream(arr).max();
         int max = maxNumber.getAsInt();
         List<Integer> fibonacciSeries = fibonacciSeries(max);
-        System.out.println("fibonacciSeries ===> "+fibonacciSeries);
         List<Integer> subSetList = Arrays.stream(arr).filter(value -> fibonacciSeries.contains(value)).boxed().collect(
             Collectors.toList());
-        System.out.println(subSetList);
+        return subSetList;
     }
 
     private static List<Integer> fibonacciSeries(int num) {
